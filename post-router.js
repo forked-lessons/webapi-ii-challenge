94f7a9944db2c5ec66fbb2e472e01930f163dc12
Daniel <danielstarling4@gmail.com>
2019-03-19 13:44:20 -0700

    get:id post and delete working

--- post-router.js
+++ post-router.js
@@ -4,9 +4,33 @@ const router = express.Router();
 
 const db = require("./data/db");
 
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await db.insert(req.body);
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "There was an error while getting the posts." });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
-    post = await db.find(req.query);
+    const posts = await db.find(req.query);
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "There was an error while getting the posts." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
     res.status(200).json(post);
   } catch (error) {
     console.log(error);
@@ -16,8 +40,16 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {});
-
-router.get("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await db.remove(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "There was an error while getting the posts." });
+  }
+});
 
 module.exports = router;
